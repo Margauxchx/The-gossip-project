1eac075ca9928f7ce10744b51c113cc5ad7aa5fa
margauxchx <margaux.chaix@yahoo.fr>
2019-10-21 15:20:15 +0200

    Terminé

---
+++ lib/controller.rb
@@ -0,0 +1,17 @@
+require 'gossip'
+
+class ApplicationController < Sinatra::Base
+  
+  get '/gossips/new/' do
+    erb :new_gossip
+  end
+  
+  get '/' do
+    erb :index, locals: {gossips: Gossip.all}
+  end
+  
+  post '/gossips/new/' do
+    Gossip.new("#{params["gossip_author"]}", "#{params["gossip_content"]}").save
+    redirect '/'
+  end
+end
